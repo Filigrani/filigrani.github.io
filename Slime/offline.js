52991524ea1e929cb05ccc3bdac1abfe52d0cea2
Filigrani <filigranisb@gmail.com>
2017-10-19 03:09:46 +0300

    Slime

---
+++ Slime/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1508371603,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/slime_slime_body-sheet0.png",
+		"images/slime_slime_eyes-sheet0.png",
+		"images/slime_slime_eyes-sheet1.png",
+		"images/slime-sheet0.png",
+		"images/player_dummy-sheet0.png",
+		"images/platform-sheet0.png",
+		"images/buttonplatform-sheet0.png",
+		"media/steam summer sale 2015 monster game - background music.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"slime.scml",
+		"slime.scon"
+	]
+}
