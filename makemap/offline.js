1a9f3c185f427ed7592a20a164a1f47d3c72cc3b
Filigrani <filigranisb@gmail.com>
2019-03-21 03:39:59 +0400

    New program for making maps

---
+++ makemap/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1553125095,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/roommap-sheet0.png",
+		"images/roommap-sheet1.png",
+		"images/leftwall-sheet0.png",
+		"images/downwall-sheet0.png",
+		"images/movearrow-sheet0.png",
+		"images/spawn-sheet0.png",
+		"images/chest-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
